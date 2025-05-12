68a07cc03b0170e9217b0095080fe59950700ebe
Ram Warhekar <ramwarhekar02@gmail.com>
2025-05-13 01:28:17 +0530

    Vite Updated

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) {
-              return 'vendor_react';
-            }
-            if (id.includes('tailwindcss')) {
-              return 'vendor_tailwindcss';
-            }
-            return 'vendor';
-          }
-        }
-      }
-    }
-  }
 })
